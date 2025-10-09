84df3a3fa8dc5c8580ab2af8bf31d9c671387e33
Phu Vo <u1153426@umail.usq.edu.au>
2025-10-09 22:25:34 +1000

    :hammer: feat: Modify error message for date of birth validation

--- src/main/java/com/learnspring/hello_spring/exception/ErrorCode.java
+++ src/main/java/com/learnspring/hello_spring/exception/ErrorCode.java
@@ -8,13 +8,13 @@ import org.springframework.http.HttpStatusCode;
 public enum ErrorCode {
     UNCATEGORIZED_EXCEPTION(999, "Uncategorized error", HttpStatus.INTERNAL_SERVER_ERROR),
     USER_EXISTS(1001, "User existed", HttpStatus.BAD_REQUEST),
-    USERNAME_INVALID(1002, "Username must be at least 3 characters long", HttpStatus.BAD_REQUEST),
-    INVALID_PASSWORD(1003, "Password must be at least 8 characters long", HttpStatus.BAD_REQUEST),
+    USERNAME_INVALID(1002, "Username must be at least {min} characters long", HttpStatus.BAD_REQUEST),
+    INVALID_PASSWORD(1003, "Password must be at least {min} characters long", HttpStatus.BAD_REQUEST),
     INVALID_KEY(1004, "Invalid key", HttpStatus.BAD_REQUEST),
     USER_NOT_EXISTED(1005, "User not existed", HttpStatus.NOT_FOUND),
     UNAUTHENTICATED(1006, "Unauthenticated", HttpStatus.UNAUTHORIZED),
     UNAUTHORIZED(1007, "You do not have permissions", HttpStatus.FORBIDDEN),
-    INVALID_DOB(1008, "Invalid date of birth", HttpStatus.BAD_REQUEST)
+    INVALID_DOB(1008, "Your age must be at least {min}", HttpStatus.BAD_REQUEST)
     ;
     private int code;
     private String message;
