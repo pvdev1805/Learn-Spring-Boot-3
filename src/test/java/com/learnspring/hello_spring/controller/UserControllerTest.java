2e047cb7e667353960e83ed89000265f0a7e2b44
Phu Vo <u1153426@umail.usq.edu.au>
2025-10-29 00:01:56 +1000

    :white_check_mark: feat: Apply h2 database for Isolation in Unit Test

--- src/test/java/com/learnspring/hello_spring/controller/UserControllerTest.java
+++ src/test/java/com/learnspring/hello_spring/controller/UserControllerTest.java
@@ -16,6 +16,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -26,6 +27,7 @@ import java.time.LocalDate;
 @SpringBootTest
 @AutoConfigureMockMvc
 @Slf4j
+@TestPropertySource("/test.properties")
 public class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
--- src/test/java/com/learnspring/hello_spring/service/UserServiceTest.java
+++ src/test/java/com/learnspring/hello_spring/service/UserServiceTest.java
@@ -12,11 +12,13 @@ import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import java.time.LocalDate;
 
 @SpringBootTest
+@TestPropertySource("/test.properties")
 public class UserServiceTest {
     @Autowired
     UserService userService;
